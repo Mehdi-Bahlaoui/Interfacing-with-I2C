a5ed02009b2fcaefbdc4cd36511303fa247a358e
mehdi <mehdibahlaoui3@gmail.com>
2025-01-28 20:19:39 +0100

    improved readablility and  removed arduino library from main sketch

--- src/LCD/Arduino.cpp
+++ src/LCD/Arduino.cpp
@@ -1,20 +1,12 @@
-#include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-void setup(){
+int main(){
 
     lcd.begin(20, 4); // initialize the lcd for 20 chars 4 lines and turn on backlight
-
-    // NOTE: Cursor Position: (CHAR, LINE) start at 0
     lcd.setCursor(0, 0);
+
     lcd.print("Hello, world!");
-    delay(1000);
-
-}
-
-void loop(){
-    // Do nothing here...
 }
---
+++ src/steps.md
@@ -0,0 +1,4 @@
+- test program than link with capteurs
+
+- remove link w/ librairie (test with saved hello sketch)
+- link with capteurs
